b763ed099fb7fb2f6afb711ff612cb81f7386665
Zarif Muhtasim <2502.zarif@gmail.com>
2024-07-29 18:20:47 +0600

    delete nvim config

--- .config/nvim/lua/plugins/kanagawa.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "rebelot/kanagawa.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- load the colorscheme here
-    vim.cmd.colorscheme "kanagawa-dragon"
-  end,
-}
